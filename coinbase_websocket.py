57e5e8409887e4f77689f5a3108585658e4eff08
Peter Calvanelli <peter.j.calvanelli@gmail.com>
2024-04-21 16:37:27 -0400

    Secret management v3

--- coinbase_websocket.py
+++ coinbase_websocket.py
@@ -2,8 +2,8 @@ from coinbase.websocket import WSClient
 import threading
 import json
 
-api_key = 'organizations/73f00023-8bb5-4758-ba5e-b63764d95792/apiKeys/ae15b8b0-24c5-409e-844b-46e4f1625981'
-api_secret = '' # See the .env file locally
+api_key = '' # See the .env file locally or add your own from Coinbase Pro
+api_secret = '' # See the .env file locally or add your own from Coinbase Pro
 
 class BitcoinPriceFetcher:
     def __init__(self, api_key, api_secret):
